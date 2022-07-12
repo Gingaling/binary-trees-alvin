b59d66dfbe598f9b68eeccf72024c10f7643857b
Gingaling <nycgingsf@hotmail.com>
2022-07-12 16:27:04 -0700

    Committing my solution code to binary-trees-my-code.js

--- binary-trees-my-code.js
+++ binary-trees-my-code.js
@@ -127,9 +127,7 @@ c.right = f;
 
 function treeIncludes(root, target) {
 	if (root === null) return [];
-
-	let queue = [root];
-
+	const queue = [root];
 	while (queue.length > 0) {
 		const current = queue.shift();
 		if (current.val === target) return true;
@@ -141,3 +139,140 @@ function treeIncludes(root, target) {
 }
 
 treeIncludes(a, "e"); // -> true
+
+// Tree Includes, however, this time implemented recursively depth first
+
+function treeIncludes(root, target) {
+    if (root === null) return false;
+    if (root === target) return true;
+    return treeIncludes(root.left, target) || treeIncludes(root.right, target);
+}
+
+// Tree Sum - Write a function, treeSum, that takes in the root of a binary tree that contains number values. The function should return the total sum of all values in the tree
+
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(-2);
+const f = new Node(1);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//       3
+//    /    \
+//   11     4
+//  / \      \
+// 4   -2     1
+
+function treeSum(root) {
+    if (root === null) return 0;
+    return root.value + treeSum(root.left )+ treeSum(root.right);
+}
+
+
+treeSum(a); // -> 21
+
+// Tree Sum - depth first, iterative
+
+function treeSum(root) {
+    if (root === null) return 0;
+    let totalSum = 0;
+    const queue = [ root ];
+
+    while (queue.length > 0) {
+        const current = queue.shift();
+        totalSum += current.val;
+        if (current.left !== null) queue.push(current.left);
+		if (current.right !== null) queue.push(current.right);
+    }
+    return totalSum;
+}
+
+// Tree Min Value - Write a function, treeMinValue, that takes in the root of a binary tree that contains number values. The function should return the minimum value within the tree.
+
+// You may assume that the input tree is non-empty.
+
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(-2);
+const f = new Node(1);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//       3
+//    /    \
+//   11     4
+//  / \      \
+// 4   -2     1
+
+// Depth First, iterative
+
+function treeMinValue(root) {
+    let smallest = Infinity;
+    const stack = [ root ];
+
+    while (stack.length > 0) {
+        const current = stack.pop();
+        if (current.val < smallest) smallest = current.val;
+
+        if (current.left !== null) queue.push(current.left);
+		if (current.right !== null) queue.push(current.right);
+    }
+    return smallest;
+}
+
+treeMinValue(a); // -> -2
+
+// Same as above, except coded recursively
+
+function treeMinValue(root) {
+    if (root === null) return Infinity;
+    const leftMin = treeMinValue(root.left);
+    const rightMin = treeMinValue(root.right);
+    return Math.min(root.val, leftMin, rightMin);
+}
+
+// Max Root-to-Leaf Path Sum - Write a function, maxPathSum, that takes in the root of a binary tree that contains number values. The function should return the maximum sum of any root to leaf path within the tree.
+
+// You may assume that the input tree is non-empty
+
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(-2);
+const f = new Node(1);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//       3
+//    /    \
+//   11     4
+//  / \      \
+// 4   -2     1
+
+// Recursive solution
+
+function maxPathSum(root) {
+    if (root === null) return -Infinity;
+    if (root.left === null  && root.right === null) return root.val;
+    const maxChildPathSum = Math.max(maxPathSum(root.left), maxPathSum(root.right));
+    return root.val + maxChildPathSum;
+}
+
+maxPathSum(a); // -> 18
